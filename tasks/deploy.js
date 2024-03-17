4a2891c5b8f23b11fd8fd7217390abfc1cf8440e
ignaciogarcia <ig@pep.security>
2024-03-17 05:54:27 +0100

    Generate one-line abi file as well as address file

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -8,7 +8,7 @@ task("deploy", "Runs a custom script with parameters")
     try {
         await runTask(taskArgs, hre)
     } catch (error) {
-        console.error('Error occurred:', '\x1b[31m', error.message, '\x1b[0m');
+        console.error('Error occurred:', '\x1b[31m', error, '\x1b[0m');
         process.exit(1);
     }
 });
@@ -25,15 +25,20 @@ async function runTask(taskArgs, hre) {
       if (!fs.existsSync(addressPathFile)) {
         throw new Error(`file ${addressPathFile} is missing`);
       }
-      const addressPath = fs.readFileSync(addressPathFile, 'utf8').trim();
+      const basePath = fs.readFileSync(addressPathFile, 'utf8').trim();
+      const addressPath = basePath + "/Address.kt"
+      const abiPath = basePath + "/Abi.kt"
       console.log("ADDRESS PATH:", addressPath);
       for (const contractName of contractNames) {
           console.log(contractName);
-          await deployContractAndSaveAddressToFile(deployer, contractName, addressPath)
+          const contractAddress = await deployContract(deployer, contractName)
+          saveContractAddressToFrontend(contractName, contractAddress, addressPath);
+          const oneLineAbi = getOneLineAbi(contractName, abiPath);
+          saveContractAbiToFrontend(contractName, oneLineAbi, abiPath);
       }
 }
 
-async function deployContractAndSaveAddressToFile(deployer, contractName, addressPath) {
+async function deployContract(deployer, contractName) {
     console.log(
         "Deploying the contract:", contractName,
         "\nwith the account:", await deployer.getAddress()
@@ -43,7 +48,7 @@ async function deployContractAndSaveAddressToFile(deployer, contractName, addres
   await contract.deployed();
 
   console.log("Contract address:", contract.address);
-  saveContractAddressToFrontend(contractName, contract.address, addressPath);
+  return contract.address
 }
 
 function saveContractAddressToFrontend(contractName, contractAddress, addressPath) {
@@ -51,7 +56,7 @@ function saveContractAddressToFrontend(contractName, contractAddress, addressPat
     console.log(ktConstantDeclaration)
     const newLine = `${ktConstantDeclaration}"${contractAddress}"`;
     if (fs.existsSync(addressPath)) {
-        console.log('The file exists.');
+        console.log('Address file exists.');
         const addressFileText = fs.readFileSync(addressPath, 'utf8').trim();
         const lines = addressFileText.split("\n");
         const filteredLines = lines.filter(item => !item.startsWith(ktConstantDeclaration));
@@ -61,11 +66,44 @@ function saveContractAddressToFrontend(contractName, contractAddress, addressPat
         console.log(newText);
         fs.writeFileSync(addressPath, newText);
     } else {
+        console.log('Address file does not exist, creating...');
         const package = getPackageLine(addressPath);
         console.log(package);
         const newText = package + "\n\n" + newLine;
         console.log(newText);
         fs.writeFileSync(addressPath, newText);
+    }
+}
+
+function getOneLineAbi(contractName, abiPath) {
+    const jsonFilePath = path.join(__dirname, '..', 'artifacts', 'contracts', `${contractName}.sol`, `${contractName}.json`);
+    const rawContract = fs.readFileSync(jsonFilePath, 'utf8');
+    const fileContract = JSON.parse(rawContract)
+    const abi = fileContract.abi
+    return JSON.stringify(abi);
+}
+
+function saveContractAbiToFrontend(contractName, oneLineAbi, abiPath) {
+    const ktConstantDeclaration = "const val " + contractName.replace(/([A-Z])/g, '_$1').toUpperCase().slice(1)+"_ABI = ";
+    console.log(ktConstantDeclaration)
+    const newLine = `${ktConstantDeclaration}"""${oneLineAbi}"""`;
+    if (fs.existsSync(abiPath)) {
+        console.log('Abi file exists.');
+        const abiFileText = fs.readFileSync(abiPath, 'utf8').trim();
+        const lines = abiFileText.split("\n");
+        const filteredLines = lines.filter(item => !item.startsWith(ktConstantDeclaration));
+        const filteredText = filteredLines.join("\n");
+        console.log("filtered text:", filteredText);
+        const newText = filteredText + "\n" + newLine;
+        console.log(newText);
+        fs.writeFileSync(abiPath, newText);
+    } else {
+        console.log('Abi file does not exist, creating...');
+        const package = getPackageLine(abiPath);
+        console.log(package);
+        const newText = package + "\n\n" + newLine;
+        console.log(newText);
+        fs.writeFileSync(abiPath, newText);
     }
 }
 
